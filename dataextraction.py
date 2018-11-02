8bff4c3a794bfc4f2816578fe88b9a6676bf978b
Besh Péres <39432749+Besh-Perez@users.noreply.github.com>
2018-11-02 20:48:32 +0000

    Update dataextraction.py

--- dataextraction.py
+++ dataextraction.py
@@ -8,34 +8,35 @@ class Store:
         self.week = week
 
 #funtion to check lists are compatible by first cheching the length on the list are the ame, 
-def check(sx, my):
+def check(sx, my, key, k1):
     a = len(sx), b = len(my)
-    nonm = list(), med = list()
-    while a == b:
-        for i in range(a):
-            sn = sx[i][0].lower(), mn = my[i][0].lower()
-            while sn == mn: c = len(sx[i])
+    nonm = list(), med = list() 
+    while a == b: #because the way i have iterated it they should be the same length
+        for i in range(a): #could of been b didn't matter
+            sn = sx[i][0].lower(), mn = my[i][0].lower() 
+            while sn == mn:
+                c = len(sx[i])
                 for j in range(1, c):
                     for k in range(2):
                         week = my[i][j][k]
                         if not week.isnum():
                             s = str(J)
-                            if s[-1] == 3:
+                            if s[-1] == "3":
                                 col = s + "rd"
-                            elif s[-1] == 2:
+                            elif s[-1] == "2":
                                 col = s + "nn"
-                            elif s[-1] == 1:
+                            elif s[-1] == "1":
                                 col = s + "st"
                             else:
                                 col = s + "th"
-                                print(f"file media has an error on row {i}, column {col}") 
+                                print(f"file media has an error on row {i}, on the {col} column") 
                         elif week == 0:
                             temp = sx[i][key - 12: key + 3]
                             nonm.append(temp)
                         else:
                             if key1 < key:
                                 print("check correct weeks have been input on row {i} for media start and finish") 
-                            temp = sx[i][key - 12: key1 - key]
+                            temp = sx[i][key - 12: key + (key1 - key)]
                             med.append(temp)
                         med.append(nonm)
                 return med #a list that sperates my data so the 0 index is media sales, then in that list it contains string for store, 
@@ -44,11 +45,11 @@ def check(sx, my):
 #function to create a list within a list to iterate though the week values easier
 def liststore(sheets, rnum, calpha):
     v1 = list(), v2 = list(), v3 = list()
-    for i in range(2, rnum + 1):
-        for j in range(2, calpha + 1):
+    for vi in range(2, rnum + 1):
+        for vj in range(2, calpha + 1):
             storename = sheets.ceel(i, 1), v2 == [stroename, [value, sheets.cell(i, j + 1)]]
-            if sheets.cell(i, 1) == sheets.cell(i - 1, 1):
-                temp = "", value = sheet.cell(i, j)
+            if sheets.cell(vi, 1) == sheets.cell(vi - 1, 1):
+                temp = "", value = sheet.cell(vi, vj)
                 if store == NULL:
                     temp = 0
                 elif store.isnum():
@@ -69,8 +70,8 @@ def liststore(sheets, rnum, calpha):
 #function to check if file titles are correct
 def correct(cellname, version):
     name = list() 
-    for i in cellname:
-        name += i.title()    
+    for icn in cellname:
+        name += icn.title()    
     if name == version:
         return True
     else:
@@ -79,15 +80,16 @@ def correct(cellname, version):
 #function to automate what are in the title cells for each spreadsheet    
 def titlemaker(sheet, col):
     cellnames = list()
-    for i in range(1, col + 1):
-        temp = sheet.cell(1, i)
+    for inc in range(1, col + 1):
+        temp = sheet.cell(1, inc)
         if not temp.isalpha():
             print("invalid entry on file")
             break
         else:
             cellnames += temp
     return cellnames
-#xyz & xyz1 = readable[0] or readable[1]
+
+#xyz & xyz1 = readable[mediaside] or readable[nonside]
 def readSale(xyz):
     abc = len(xyz)
     for irsale in range(abc):
@@ -96,7 +98,9 @@ def readSale(xyz):
         else:
             xrs = len(xyz[irsale])
             for jrsale in range(1, xrs):
-                sale = xyz[i][j][1::2]
+                sale = xyz[i][j][1::2] # [1::2] means only get the values on the right of the right
+                if sale.isnum():
+                    sale = int(sale)
                 return sale
             
 def readYAXIS(xyz2):
@@ -104,7 +108,7 @@ def readYAXIS(xyz2):
     for irweek in range(abc2):
         xrw = len(xyz[irweek])
         for jrweek in range(1, xrw):
-            yA = xyz2[irweek][jrweek][::2]
+            yA = xyz2[irweek][jrweek][::2] # means values on left
             return yA
         
  def readXAxis(xyz1):
@@ -134,9 +138,19 @@ def writeSale(yz, sheetyz):
     for w1i in range(1, zz + 1):
         az = len(zz[w1i])
         for w1j in range(1, az + 1):
-            coords = sheetyz.cell(w1i, w1j)
-            return coords
+            return sheetyz.cell(w1i, w1j).value = 
+        
+def whichsheet(argx, as1, as2):
+    if argx == readable[mediaside]:
+        Wsheet = asheet
+    elif argx == readable[nonside]:
+        Wsheet = asheet1
+    else:
+        print(f"unknownn variable {argx} entered")
+        break
+    return Wsheet
 
+writeSale(readSale(argx, Wsheet)), writeAxis(readYAxis(argx, Wsheet)), writeAxis(readYaxis(argx, Wsheet)))        
 #creating a variable to check if true or false
 mc = correct(titlemaker(mhm, mcolumns), campaignform), sc = correct(titlemaker(shm, scolumns), salesform)
 
@@ -156,6 +170,7 @@ mrows = shm.max_row, mcolumns = shm.max_column
 campaignform = ["Store", "Start", "Finish"]
 salesform = ["Store", "Week", "Sales"]
 
+#making the data readable for me the user
 if mc not True:
     print(f"ERROR: file {campaign} doesn't appear to be formatted correctly")
     break
@@ -163,10 +178,8 @@ elif sc not True:
     print(f"ERROR: file {sales} doesn't appear to be formatted correctly")
     break
 else:
-    uglystore = liststore(shs, srows, scolumns), uglymedia = liststore(shm, mrows, mcolumns)
-
-#preparing to read the data in a format im more comfortable with
-readable = check(uglystore, uglymedia)
+    readable = check(liststore(shs, srows, scolumns), uglymedia = liststore(shm, mrows, mcolumns))
+    mediaside = 0, nonside = 1
 
 #preparing my sheets and naming them
 wk = openpyxl.Workbook()
@@ -176,7 +189,7 @@ asheet.title = "Media Performance"
 #naming extra sheets in range of 3 because i am creating 3 new sheets.
 for sheetnum in range (3):
     if shhetnum == 0:
-        sheetTitle = "Analysis", key = 1
+        sheetTitle = "Analysis"
         sheet0 = sheetTitle
     elif sheetnum == 1:
         sheetTitle = "Non-Media Performance"
@@ -185,13 +198,9 @@ for sheetnum in range (3):
         sheetTitle = "Visulisation"
         sheet2 = sheetTitle
     wk.create_sheet = (title = sheetTitle)
-    
-while writeSale(readSale(readable[0]), asheet):
-    wtiretosh += Axis
-writetoshn = writeAxis(readYaxis(readable[0], asheet)
-writetosh += writetoshn
-writetoshw = writeAxis(readXAxis(readable[0], asheet)
 
+
+                                                                                             
 brandname = input("please enter the name of the product you intend to be using here : ")
 #saving file
-beta.save(f"C:\\{brand}anaylsis.xlsx")
+wk.save(f"C:\\{brand}anaylsis.xlsx")
